71ec12ef2b723da9243debe6725929203cfc18a0
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-25 03:19:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    puts "Welcome to my app!"
-   puts  "Isn't this cool?"
-    puts "I think so too!"
+  "Hey y'all"
   end
 
 end
